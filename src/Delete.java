2ec3f3a2456aaaef88eeaacfe71ac1106375710b
Aman-20032003 <amankumaran2003>
2024-01-30 13:21:58 +0530

    update

---
+++ src/Delete.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Delete {
+  static BufferedReader bufferedReader= new BufferedReader(new InputStreamReader(System.in));
+    public static void delete() throws IOException {
+        System.out.println("Enter  Your Name For Deletion");
+        String name = bufferedReader.readLine();
+        if (){
+
+        }else {
+
+        }
+
+
+    }
+}
