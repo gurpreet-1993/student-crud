c1b7ff1250c06f2e902ee790bb7e35a35581e469
Ayush-pailoss <pailossayush@gmail.com>
2024-01-30 13:23:04 +0530

    updated

---
+++ src/Update.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Update {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void updation(){
+        System.out.println("enter name :");
+        try {
+            String name = br.readLine();
+        }
+        catch (Exception ex){
+            System.out.println("invalid input");
+        }
+
+    }
+}
